c3575b111d0f46a4494aec08875ba37a3d58421a
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2023-03-22 15:25:05 +0000

    Update 200_Rest.sh

--- 200_Rest.sh
+++ 200_Rest.sh
@@ -30,10 +30,11 @@ cp zentile_linux_amd64 ~/.config/zentile_linux_amd64
 
 sudo bash -c "$(curl -fsSL https://git.io/JsADh || wget -q https://git.io/JsADh -O -)"
 
-pacstall -I balena-etcher-deb
+#pacstall -I balena-etcher-deb
 pacstall -I pycharm-community-bin
 #pacstall -I nerd-fonts-jetbrains-mono
 #pacstall -I alacritty
 #pacstall -I zoom-deb
 sudo apt install python3-tk python3-pil python3-pil.imagetk sqlitebrowser python3-pip
+sudo apt install balena-etcher
 ~/.config/zentile_linux_amd64 &
